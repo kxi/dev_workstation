c8502f8f0a9d8302874a98e515ac54483bcb733e
heroxk <kaixi.life@gmail.com>
2016-01-30 13:44:46 +1100

    Ubuntu Done

---
+++ Vagrantfile
@@ -0,0 +1,27 @@
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.define "dev-desktop" do |desktop|
+    desktop.vm.box = "ubuntu/trusty64"
+#  config.vm.box = "bento/centos-6.7"
+    desktop.ssh.forward_agent = true
+    desktop.ssh.forward_x11 = true
+    desktop.vm.provider "virtualbox" do |vb|
+      # Display the VirtualBox GUI when booting the machine
+      vb.gui = true
+      vb.memory = 2048
+      vb.cpus = 1
+    end
+   
+  # Install xfce and virtualbox additions
+    desktop.vm.provision "shell", path: "files/install_script.sh"
+#  config.vm.provision "shell", inline: "sudo apt-get install -y xfce4 virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11"
+  # Permit anyone to start the GUI
+#  config.vm.provision "shell", inline: "sudo sed -i 's/allowed_users=.*$/allowed_users=anybody/' /etc/X11/Xwrapper.config"
+
+#  config.vm.network "forwarded_port", guest: 20, host: 1020
+    desktop.vm.network "forwarded_port", guest: 80, host: 1080
+#  config.vm.network "forwarded_port", guest: 443, host: 8443
+  end
+end
+
---
+++ files/install_script.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+export UserID=rubin
+cd /tmp/
+sudo apt-add-repository -y ppa:ubuntu-mate-dev/ppa
+sudo apt-add-repository -y ppa:ubuntu-mate-dev/trusty-mate
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get -y update
+
+# Install Desktop
+sudo apt-get -y install --no-install-recommends ubuntu-mate-core ubuntu-mate-desktop
+sudo init 5
+
+# Install JDK
+echo 'oracle-java8-installer shared/accepted-oracle-license-v1-1 select true' | sudo debconf-set-selections
+sudo apt-get -y install oracle-java8-installer
+
+# Install Eclipse
+sudo tar -zxvf /vagrant/files/eclipse-jee-mars-1-linux-gtk-x86_64.tar.gz -C /opt/
+cp /vagrant/files/eclipse.desktop /usr/share/applications/
+sudo desktop-file-install /usr/share/applications/eclipse.desktop
+sudo ln -s /opt/eclipse/eclipse /usr/local/bin/
+
+# Install MAVEN
+export MAVEN_VERSION=3.3.3
+wget https://archive.apache.org/dist/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz
+tar zxvf apache-maven-$MAVEN_VERSION-bin.tar.gz
+sudo mv /tmp/apache-maven-$MAVEN_VERSION /usr/share/maven 
+sudo ln -s /usr/share/maven/bin/mvn /usr/bin/mvn
+sudo useradd -s /bin/bash -m -d /home/$UserID $UserID
+echo "${UserID}:${UserID}" | sudo chpasswd
+echo "${UserID}    ALL=(ALL:ALL) ALL" | sudo tee -a /etc/sudoers > /dev/null
+
+# Install Git
+export GIT_VERSION=2.6.5
+sudo apt-get install autoconf libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev 
+wget https://github.com/git/git/archive/v${GIT_VERSION}.zip 
+unzip v${GIT_VERSION}.zip 
+cd /tmp/git-${GIT_VERSION}
+make configure
+./configure --prefix=/usr
+make all 
+sudo make install
+
+# Install SmartGit
+export Smartgit_VERSION=7_0_5
+cp /vagrant/files/smartgit-generic-${Smartgit_VERSION}.tar.gz /tmp/
+tar zxvf smartgit-generic-${Smartgit_VERSION}.tar.gz
+sudo mkdir -p /usr/local/smartgit/${Smartgit_VERSION}
+sudo cp -r /tmp/smartgit/* /usr/local/smartgit/${Smartgit_VERSION}/
+sudo ln -s /usr/local/smartgit/${Smartgit_VERSION}/bin/smartgit.sh /usr/bin/smartgit
+
+reboot
+
