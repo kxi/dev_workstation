6e076c6a4f2b93abe812a230e4dd54e9660de37a
Kai Xi <kaixi@Kais-MacBook-Pro.local>
2016-02-02 07:58:20 +1100

    Fix Eclipse Installation Issue

--- files/install_script.sh
+++ files/install_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export UserID=rubin
 cd /tmp/
+cp /vagrant/files/* /tmp/
 sudo apt-add-repository -y ppa:ubuntu-mate-dev/ppa
 sudo apt-add-repository -y ppa:ubuntu-mate-dev/trusty-mate
 sudo add-apt-repository -y ppa:webupd8team/java
@@ -15,8 +16,8 @@ echo 'oracle-java8-installer shared/accepted-oracle-license-v1-1 select true' | 
 sudo apt-get -y install oracle-java8-installer
 
 # Install Eclipse
-sudo tar -zxvf /vagrant/files/eclipse-jee-mars-1-linux-gtk-x86_64.tar.gz -C /opt/
-cp /vagrant/files/eclipse.desktop /usr/share/applications/
+sudo tar -zxvf /tmp/eclipse-jee-mars-R-linux-gtk-x86_64.tar.gz -C /opt/
+cp /tmp/eclipse.desktop /usr/share/applications/
 sudo desktop-file-install /usr/share/applications/eclipse.desktop
 sudo ln -s /opt/eclipse/eclipse /usr/local/bin/
 
@@ -31,8 +32,9 @@ echo "${UserID}:${UserID}" | sudo chpasswd
 echo "${UserID}    ALL=(ALL:ALL) ALL" | sudo tee -a /etc/sudoers > /dev/null
 
 # Install Git
+cd /tmp/
 export GIT_VERSION=2.6.5
-sudo apt-get install autoconf libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev 
+sudo apt-get -y install autoconf libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev 
 wget https://github.com/git/git/archive/v${GIT_VERSION}.zip 
 unzip v${GIT_VERSION}.zip 
 cd /tmp/git-${GIT_VERSION}
@@ -43,14 +45,14 @@ sudo make install
 
 # Install SmartGit
 export Smartgit_VERSION=7_0_5
-cp /vagrant/files/smartgit-generic-${Smartgit_VERSION}.tar.gz /tmp/
+wget http://www.syntevo.com/downloads/smartgit/smartgit-generic-${Smartgit_VERSION}.tar.gz
 tar zxvf smartgit-generic-${Smartgit_VERSION}.tar.gz
 sudo mkdir -p /usr/local/smartgit/${Smartgit_VERSION}
 sudo cp -r /tmp/smartgit/* /usr/local/smartgit/${Smartgit_VERSION}/
-sudo ln -s /usr/local/smartgit/${Smartgit_VERSION}/bin/smartgit.sh /usr/bin/smartgit
+sudo ln -sfn /usr/local/smartgit/${Smartgit_VERSION}/bin/smartgit.sh /usr/bin/smartgit
 
 # Install Workbench
-sudo apt-get install mysql-workbench
+sudo apt-get -y install mysql-workbench
 
 reboot
 
